7599e25910b6dffe01f79725c51ebe3692d5db7b
Jade Lynn Masker <hopefulfire@gmail.com>
2020-01-04 10:45:58 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    #splits into sentences
+    sentences = self.split('.').join('__s__').split('?').join('__s__').split('!').join('__s__').split('__s__')
+    #removes empty sentences
+    sentences.delete_if do |sentence|
+      sentence == ''
+    end
+    #returns num of sentences
+    sentences.length
   end
 end
